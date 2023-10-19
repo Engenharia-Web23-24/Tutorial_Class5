afab7308381bd9eab4d0e789677a8c36dbe94e00
lfbutad <lfb@utad.pt>
2023-10-19 22:48:47 +0100

    Tutorial Class05 implementation

---
+++ Class05/Data/Class05Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Class05.Models;
+
+namespace Class05.Data
+{
+    public class Class05Context : DbContext
+    {
+        public Class05Context (DbContextOptions<Class05Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Class05.Models.Category> Category { get; set; } = default!;
+
+        public DbSet<Class05.Models.Course>? Course { get; set; }
+    }
+}
